dcbb8ff0a233a341bb2f4171f9399362154b81c2
William Papper <contact@papper.me>
2016-12-07 09:25:20 -0800

    Filled out basics.js

--- js/basics.js
+++ js/basics.js
@@ -1,10 +1,16 @@
 var titles = [
   'Breathing Meditation',
-  'Loving Kindness Meditation'
+  'Loving Kindness Meditation',
+  'Body Scan Meditation',
+  'Body and Sound Meditation',
+  'Breath, Sound, Body Meditation'
 ]
 var links = [
   'http://marc.ucla.edu/mpeg/01_Breathing_Meditation.mp3',
-  'http://marc.ucla.edu/mpeg/05_Loving_Kindness_Meditation.mp3'
+  'http://marc.ucla.edu/mpeg/05_Loving_Kindness_Meditation.mp3',
+  'http://marc.ucla.edu/mpeg/Body-Scan-Meditation.mp3',
+  'http://marc.ucla.edu/mpeg/Body-Sound-Meditation.mp3',
+  'http://marc.ucla.edu/mpeg/02_Breath_Sound_Body_Meditation.mp3'
 ]
 
 $(document).ready(function() {
