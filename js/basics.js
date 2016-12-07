f0f04adfb519b2c864202bd92decc22f8b169b76
William Papper <contact@papper.me>
2016-12-07 09:11:25 -0800

    Added a template for displaying basic meditations

--- js/basics.js
+++ js/basics.js
@@ -1,5 +1,17 @@
+var titles = [
+  'Breathing Meditation',
+  'Loving Kindness Meditation'
+]
+var links = [
+  'http://marc.ucla.edu/mpeg/01_Breathing_Meditation.mp3',
+  'http://marc.ucla.edu/mpeg/05_Loving_Kindness_Meditation.mp3'
+]
+
 $(document).ready(function() {
   $("#another")[0].addEventListener('click', function() {
-    $("#option").text("" + Math.random());
+  var itemNumber = Math.floor(Math.random()*titles.length);
+  console.log(itemNumber)
+    $("#option").text(titles[itemNumber]);
+    $("#option").attr("href", links[itemNumber]);
   }, false);
 });
